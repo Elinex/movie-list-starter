777f2baad6048cfe94b09adb271d96c4a47c70d7
Elinex <eline.ax@gmail.com>
2019-06-22 10:59:18 -0700

    add isWatched as props to MovieList

--- src/MovieList.js
+++ src/MovieList.js
@@ -3,17 +3,20 @@ import React from 'react';
 import MovieListItem from './MovieListItem'
 
 const MovieList = (props) => {
-    return (
-        <ul>
-            {props.movies.map(movie => (
-                <MovieListItem 
-                    movie={movie} 
-                    key={movie.title} 
-                    isWatched={props.isWatched}
-                />
-            ))}
-        </ul>
-    )
+  return (
+    <div>
+      <div>{props.isWatched ? 'Watched' : 'To Watch'}</div>
+      <ul>
+        {props.movies.map(movie => (
+          <MovieListItem 
+            movie={movie} 
+            key={movie.title} 
+            isWatched={props.isWatched}
+          />
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default MovieList;
