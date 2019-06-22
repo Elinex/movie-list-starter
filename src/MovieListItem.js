44a62c728b9de80efabe3393f5ba1f5f46891b14
Elinex <eline.ax@gmail.com>
2019-06-22 16:00:00 -0700

    make appear a new template when click movie title

--- src/MovieListItem.js
+++ src/MovieListItem.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
-const MovieListItem = (props) => (
-    <li>
-        {props.movie.title}
-        <button onClick={() => props.changeMovieWatched(props.movie)} >
-            {props.isWatched ? 'Watched' : 'To Watch'}
+class MovieListItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showMovieInfo: false
+    }
+    this.changeMovieInfo = this.changeMovieInfo.bind(this);
+  };
+
+  changeMovieInfo () {
+    this.setState(state => {
+      return {
+        showMovieInfo: !state.showMovieInfo
+      }
+    })
+  }
+
+  render () {
+    const templateMovieInfo = (
+      <p>
+        hello
+      </p>
+    );
+
+    return (
+      <li>
+        <p onClick={() => this.changeMovieInfo()}>
+          {this.props.movie.title}
+        </p>
+        {this.state.showMovieInfo && templateMovieInfo}
+        <button onClick={() => this.props.changeMovieWatched(this.props.movie)} >
+          {this.props.isWatched ? 'Watched' : 'To Watch'}
         </button>
-    </li>
-)
+      </li>
+    )
+  }
+
+}
 
 export default MovieListItem;
